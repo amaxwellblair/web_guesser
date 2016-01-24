ab3cf2729e70f1853a22c3f60d7e9c75fe194bc4
Allan <amaxwellblair@gmail.com>
2016-01-23 18:49:39 -0700

    completed dirty cheater extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,16 +5,24 @@ RANDOM_NUMBER = rand(0..100)
 output = ""
 get '/' do
     output = guesswork(params['guess'])
+    output += "<br/>You dirty cheater the secret number is: " + RANDOM_NUMBER.to_s if params['cheat'] == 'true'
     erb :index, :locals => {:random_number => RANDOM_NUMBER, :output => output}
 end
 
 def guesswork(guess)
   if guess.to_i == RANDOM_NUMBER
-    output = "You win! The secret number is #{RANDOM_NUMBER}"
+    "You win! The secret number is #{RANDOM_NUMBER}"
   elsif guess.to_i > RANDOM_NUMBER
-    output = "Too high"
+    if guess.to_i - RANDOM_NUMBER > 5
+      "Way too high"
+    else
+      "Too high"
+    end
   elsif guess.to_i < RANDOM_NUMBER
-    output = "Too low"
+    if guess.to_i - RANDOM_NUMBER < -5
+      "Way too low"
+    else
+      "Too low"
+    end
   end
-  return output
 end
